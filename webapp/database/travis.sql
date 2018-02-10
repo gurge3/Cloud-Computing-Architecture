63de1e6791a61c984dfb6b0ba259fd7a051ae72c
Xingyao Wu <gurge3@gmail.com>
2018-02-09 22:25:06 -0500

    update sql

--- webapp/database/travis.sql
+++ webapp/database/travis.sql
@@ -19,6 +19,7 @@
 -- Table structure for table `user`
 --
 CREATE DATABASE IF NOT EXISTS assignment2;
+use assignment2;
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
