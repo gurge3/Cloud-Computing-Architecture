72950eee11234b708b40181f573f78b7cb3d53ae
Xingyao Wu <gurge3@gmail.com>
2018-02-28 02:59:50 -0500

    update

--- applicationStart.sh
+++ applicationStart.sh
@@ -1,5 +1,7 @@
 ls -al
 cd /var/lib/tomcat8/webapps
+sudo service tomcat8 stop
+sudo nohup 'sudo java -jar ROOT.war' > java.log &
 ls -al
 cd ~/frontend
-sudo nohup ng serve > out.log &
+sudo nohup 'sudo ng serve' > out.log &
