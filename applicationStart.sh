0d0ad6e124e01a0ba904a895857a66196264913c
Xingyao Wu <gurge3@gmail.com>
2018-02-28 12:41:09 -0500

    update

--- applicationStart.sh
+++ applicationStart.sh
@@ -1,7 +1,11 @@
-ls -al
-cd /var/lib/tomcat8/webapps
 sudo service tomcat8 stop
-sudo nohup 'sudo java -jar ROOT.war' > java.log &
-ls -al
 cd ~/frontend
-sudo nohup 'sudo ng serve' > out.log &
+sudo ng build --environment=prod
+cp dist/* /var/lib/tomcat8/webapps/ROOT
+ls -al
+cd /var/lib/tomcat8/conf
+sudo sed -i 's/port="8080"/port="4200"/' server.xml
+sudo service tomcat8 start
+cd /var/lib/tomcat8/webapps
+sudo nohup sudo java -jar ROOT.war &
+ls -al
