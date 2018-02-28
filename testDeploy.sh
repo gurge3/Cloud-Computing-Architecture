eaa6499af30d164f10164ccb6e2033e5e22aa83c
Xingyao Wu <gurge3@gmail.com>
2018-02-28 13:11:09 -0500

    update

--- testDeploy.sh
+++ testDeploy.sh
@@ -2,8 +2,3 @@ ls -al
 cd /var/lib/tomcat8/webapps
 sudo echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'rootroot';" | mysql -u root
 sudo mysql -u root --password="rootroot" < travis.sql
-ls -al
-cd ~/frontend
-sudo npm install
-sudo npm install -g @angular/cli@latest
-sudo ng build --environment=prod
