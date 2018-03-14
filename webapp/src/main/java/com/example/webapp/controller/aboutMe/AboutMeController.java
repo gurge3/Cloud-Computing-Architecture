73778e6b797f4934c8f29c5333cd8ea5b983004e
Xingyao Wu <gurge3@gmail.com>
2018-03-14 16:37:04 -0400

    update

--- webapp/src/main/java/com/example/webapp/controller/aboutMe/AboutMeController.java
+++ webapp/src/main/java/com/example/webapp/controller/aboutMe/AboutMeController.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/aboutMe")
-@CrossOrigin(origins="http://ec2-34-229-214-6.compute-1.amazonaws.com:4200", maxAge = 3600)
+@CrossOrigin(origins="http://ec2-54-145-197-128.compute-1.amazonaws.com:4200", maxAge = 3600)
 public class AboutMeController {
     @Autowired
     private AboutMeService aboutMeService;
--- webapp/src/main/java/com/example/webapp/controller/user/AuthenticationController.java
+++ webapp/src/main/java/com/example/webapp/controller/user/AuthenticationController.java
@@ -10,7 +10,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/authenticate")
-@CrossOrigin(origins="http://ec2-34-229-214-6.compute-1.amazonaws.com:4200", maxAge = 3600)
+@CrossOrigin(origins="http://ec2-54-145-197-128.compute-1.amazonaws.com:4200", maxAge = 3600)
 public class AuthenticationController {
 
     @Autowired
