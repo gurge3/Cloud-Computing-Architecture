8e4e01f7575e8aa55f9e9c8dbf89c6ef0bc152c4
Xingyao Wu <gurge3@gmail.com>
2018-04-18 17:53:18 -0400

    update

--- testDeploy.sh
+++ testDeploy.sh
@@ -1,4 +1,6 @@
 ls -al
 cd /var/lib/tomcat8/webapps
+sudo export DEBIAN_FRONTEND=noninteractive
+sudo -E apt-get -q -y install mysql-server
 sudo echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'rootroot';" | sudo mysql -u root
 sudo mysql -u root --password="rootroot" < travis.sql
