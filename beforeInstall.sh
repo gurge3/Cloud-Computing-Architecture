ee15bcabbbcbb6217a065c7e9a39640461b80748
Xingyao Wu <gurge3@gmail.com>
2018-04-21 10:14:40 -0400

    update

--- beforeInstall.sh
+++ beforeInstall.sh
@@ -4,5 +4,4 @@ sudo export DEBIAN_FRONTEND=noninteractive
 debconf-set-selections <<< 'mysql-server-5.7 mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server-5.7 mysql-server/root_password_again password root'
 apt-get install
-sudo -E apt-get -q -y install mysql-server
 sudo rm -rf /var/lib/tomcat8/webapps/ROOT
