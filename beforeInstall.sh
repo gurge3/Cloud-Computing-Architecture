f671c70d43434ae7f3fb5601fa31f03576c4bead
Xingyao Wu <gurge3@gmail.com>
2018-04-21 09:55:25 -0400

    update

--- beforeInstall.sh
+++ beforeInstall.sh
@@ -1,5 +1,8 @@
 cd /var/lib/tomcat8/webapps
-sudo dpkg --configure -a
+sudo apt-get install debconf-utils
 sudo export DEBIAN_FRONTEND=noninteractive
+debconf-set-selections <<< 'mysql-server-5.7 mysql-server/root root root'
+debconf-set-selections <<< 'mysql-server-5.7 mysql-server/root root root'
+apt-get install
 sudo -E apt-get -q -y install mysql-server
 sudo rm -rf /var/lib/tomcat8/webapps/ROOT
