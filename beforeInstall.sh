b1bd5b584c081a3eb02ef265bde1e80f6637f713
Xingyao Wu <gurge3@gmail.com>
2018-04-21 09:42:43 -0400

    update

--- beforeInstall.sh
+++ beforeInstall.sh
@@ -1,2 +1,5 @@
 cd /var/lib/tomcat8/webapps
+sudo dpkg --configure -a
+sudo export DEBIAN_FRONTEND=noninteractive
+sudo -E apt-get -q -y install mysql-server
 sudo rm -rf /var/lib/tomcat8/webapps/ROOT
