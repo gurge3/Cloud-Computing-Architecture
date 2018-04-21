df7f8335a667a33bc85e01532bad1eeb85761e97
Xingyao Wu <gurge3@gmail.com>
2018-04-21 09:25:59 -0400

    update

--- testDeploy.sh
+++ testDeploy.sh
@@ -1,5 +1,6 @@
 ls -al
 cd /var/lib/tomcat8/webapps
+sudo dpkg --configure -a
 sudo export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get -q -y install mysql-server
 sudo echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'rootroot';" | sudo mysql -u root
