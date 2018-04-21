4615270d43516263adb0d220e5d886b954d726a9
Xingyao Wu <gurge3@gmail.com>
2018-04-21 10:23:47 -0400

    update

--- beforeInstall.sh
+++ beforeInstall.sh
@@ -1,7 +1,5 @@
+sudo dpkg --configure -a
+export DEBIAN_FRONTEND=noninteractive
+sudo -E apt-get -q -y install mysql-server
 cd /var/lib/tomcat8/webapps
-sudo apt-get install debconf-utils
-sudo export DEBIAN_FRONTEND=noninteractive
-debconf-set-selections <<< 'mysql-server-5.7 mysql-server/root_password password root'
-debconf-set-selections <<< 'mysql-server-5.7 mysql-server/root_password_again password root'
-apt-get install
 sudo rm -rf /var/lib/tomcat8/webapps/ROOT
--- testDeploy.sh
+++ testDeploy.sh
@@ -1,4 +1,4 @@
 ls -al
 cd /var/lib/tomcat8/webapps
-sudo echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'root' BY 'rootroot';" | sudo mysql -u root
+sudo echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'rootroot';" | sudo mysql -u root
 sudo mysql -u root --password="rootroot" < travis.sql
